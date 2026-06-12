cda5818bd3a9ff0b3a274948a2af5cc48ac63efa
purvi-chaurasia <purvi.chaurasia.2005@gmail.com>
2026-06-12 22:09:03 +0530

    Added Day 1 programs

---
+++ Day_1/ques_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("enter a number;");
+    scanf("%d", &n);
+    printf("multilication table of %d:\n",n);
+    for(i=1;i<=10;i++)
+    {
+        printf("%d * %d=%d\n",n,i,n*i);
+    }
+        return 0;       
+
+}
---
+++ Day_1/ques_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int total,count = 0;
+    printf("enter a number:");
+    scanf("%d",&total);
+    if(total==0)
+    {
+        count=1;
+    }
+    else
+    {
+        while(total!=0)
+        {
+            total=total/10;
+            count++;
+        }
+    }
+    printf("Number of digits = %d", count);
+return 0;
+}
