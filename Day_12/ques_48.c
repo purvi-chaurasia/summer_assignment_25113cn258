f52c507987c6dc7886c26e78a1ca1f8adaf4b1d2
purvi-chaurasia <purvi.chaurasia.2005@gmail.com>
2026-06-12 21:39:32 +0530

    Added Q47 Fibonacci and Q48 Perfect Number programs

---
+++ Day_12/ques_48.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void perfect(int n)
+{
+    int i, sum=0;
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        sum+=i;
+    }
+    if(sum==n)
+    printf("%d is a perfect number",n);
+    else
+    printf("%d is not a perfect number",n);
+}
+int main()
+{
+    int num;
+    printf("enter a number:");
+    scanf("%d", &num);
+    perfect(num);
+    return 0;
+}
